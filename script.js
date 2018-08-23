d8347b7cadbbc97481b3da868dfe0c31fcb4136d
kidistz <kidistmulusew@gmail.com>
2018-08-23 10:45:50 -0400

    second commit

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
 var app = angular.module("myModule",[]);
 app.controller("myController",function($http,$scope,$log){
 
+     
+
                       $http.get("https://dash-api.com/api/v3/allData.php")
                       .then(function (response) {
                       console.log('get',response)
-                      $scope.url = response.data.stations[2].stream_url;
-                     
-                       })
-                      .catch(function (data) {
-                      throw "error 1";
-                   });
-                   $http.get("http://ice55.securenetsystems.net/DASH36.m3u")
+                      $scope.stations = response.data.stations.slice(0, 5);
+                        for (let station of $scope.stations) {
+                            $http.get(station.stream_url)
                       .then(function (response) {
                       console.log('get',response.data);
                      
                         var playlist = M3U.parse(response.data);
-                      
+                        // for (let music of playlist) {
+                        //     var audio = new Audio();
+                        //     audio.src = music.file;
+                        //     music.audio = audio;
+                        // }
+                        // console.log(playlist);
+                        station.playlist = playlist;
                        })
                       .catch(function (data) {
                       throw "error 1";
                    });
+                        }
+                     
+                       })
+                      .catch(function (data) {
+                      throw "error 1";
+                   });
+                   
                 
+                   $scope.start = () => {
+                       console.log($scope.stations);
+                   }
+                //    $scope.parse=function () {
+                //     var playlist = M3U.parse(response.data);
+                //     var audio = new Audio();
+                //     next(audio, playlist, 0);
+                //   };
+
+                // $scope.next = function(audio, playlist, i) {
+                //     if (i < playlist.length) {
+                //       audio.src = playlist[i++].file;
+                //       audio.onended = next.bind(null, audio, playlist, i);
+                //       audio.play();
+                //     }
+                //   };
+                 
+
+
+
                      
                    
 
